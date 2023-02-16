0e9228f6ff667ba27d32d246865fa8b06e99ef0e
Ruby Truong <rubytruong1011@gmail.com>
2023-02-16 00:02:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,292 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" ">
+      <section className="pt-[100px] ">
+        <div className="relative w-full h-[576px] bg-[#2433A6]">
+          <div className="container relative left-2/4 -translate-x-2/4">
+            <div>
+              <div className="relative top-0 pt-[48px] pb-[48px] text-[25px] leading-[48px] flex justify-center items-center font-bold">
+                <h3 className="text-[#FFFFFF]">
+                  GIỚI THIỆU{" "}
+                  <span className="text-[#F5882E]"> CHƯƠNG TRÌNH</span>
+                </h3>
+              </div>
+              <div className="grid grid-cols-10 gap-0 relative">
+                <div className="col-span-5 relative  left-0 object-cover border-current flex justify-center items-center">
+                  <img
+                    className="absolute w-[550px] h-[350px]"
+                    src="/images/background1.png"
+                  />
+
+                  <img
+                    className="absolute bottom-0"
+                    src="/images/group11.png"
+                  />
+                </div>
+                <div className="col-span-5 w-[660px] relative flex justify-center items-center text-justify">
+                  <p className="font-mont font-normal text-[18px] pl-[20px] pr-[20px] text-[#FFFFFF]">
+                    Chương trình Thạc Sỹ Tài Chính được thiết kế và đào tạo theo
+                    định hướng thực hành, chuyên sâu về Tài chính doanh nghiệp,
+                    học viên có thể áp dụng ngay các kiến thức trong chương
+                    trình vào công việc thực tế. Bên cạnh đó, tiếng Anh là ngôn
+                    ngữ đào tạo của chương trình đã giúp học viên được trau dồi
+                    thêm vốn tiếng Anh, từ đó có nhiều cơ hội phát triển nghề
+                    nghiệp trong bối cảnh hội nhập toàn cầu.
+                    <br />
+                    <br />
+                    Giảng viên tham gia chương trình là các giảng viên của
+                    trường <strong> Đại học Jean Moulin Lyon 3 </strong> cử sang
+                    và các giảng viên của{" "}
+                    <strong>
+                      Viện Đào Tạo Quốc Tế - Trường Đại Học Thương Mại
+                    </strong>{" "}
+                    . Các giảng viên được tuyển chọn đều có kiến thức chuyên môn
+                    sâu, tham gia nhiều công trình nghiên cứu khoa học, có kinh
+                    nghiệm thực tế vững vàng và trình độ tiếng Anh tốt, cùng với
+                    lòng nhiệt huyết cao với sự nghiệp giáo dục.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pt-[40px]">
+        <div className="relative w-full  bg-[#FCFCFC]">
+          <div className="container relative left-2/4 -translate-x-2/4">
+            <div>
+              <div className="relative top-0 text-[25px] font-bold flex justify-center items-center pt-[48px] pb-[48px]">
+                <h3>
+                  <span className="text-[#2433A6]">CÁC BƯỚC</span>
+                  <span className="text-[#F5882E]"> TUYỂN SINH</span>
+                </h3>
+              </div>
+              <div className="flex relative mt-[5px]">
+                <div className="mb-[150px] mt-[100px] mr-[30px] relative z-40">
+                  <div className="w-[317px] h-[216px] bg-[#3EAED2] rounded-lg">
+                    <div>
+                      <div className="absolute w-[68px] h-[68px] bg-white rounded-full mt-[70px] ml-[80px]">
+                        <div className="absolute">
+                          <img
+                            src="/images/a1.png"
+                            className="w-[33px] h-[33px] mt-[17px] ml-[19px]"
+                          />
+                        </div>
+                      </div>
+
+                      <div className="absolute mt-[50px] ml-[200px] text-[18px] font-bold  text-[#2433A6]">
+                        <p>Bước 1</p>
+                      </div>
+
+                      <div className="absolute mt-[100px] ml-[200px] text-[18px]  text-[#FFFFFF]">
+                        <p>Đăng ký </p>
+                        <p>trực tuyến</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="-translate-x-2/4 translate-y-[58px] z-30">
+                  <div class="border-solid border-l-[#3EAED2] border-l-[150px] border-y-transparent border-y-[150px] border-r-0"></div>
+                  <div className="absolute w-[68px] h-[68px] bg-white rounded-full mt-[-200px] ml-[145px]  ">
+                    <div className="absolute">
+                      <img
+                        src="/images/a1.png"
+                        className="w-[33px] h-[33px] mt-[17px] ml-[19px]"
+                      />
+                    </div>
+                  </div>
+                  <div className="absolute mt-[-205px] ml-[230px] text-[18px] font-bold w-[70px] text-[#FFFFFF]">
+                    <p>Bước 2</p>
+                  </div>
+
+                  <div className="z-30 w-[100px] absolute  mt-[-156px] ml-[210px] text-[18px]  text-[#FFFFFF]">
+                    <p>Nộp hồ sơ</p>
+                    <p>đăng ký</p>
+                  </div>
+                </div>
+
+                <div className="mb-[150px] mt-[100px] mr-[30px] absolute translate-x-72 z-28">
+                  <div className="w-[317px] h-[216px] bg-[#2433A6] rounded-lg"></div>
+                </div>
+                <div className="translate-x-2/4 translate-y-[58px] z-30">
+                  <div class="border-solid border-l-[#2433A6] border-l-[150px] border-y-transparent border-y-[150px] border-r-0"></div>
+                </div>
+                <div className="mb-[150px] mt-[100px] mr-[30px] -translate-x-[40px] relative">
+                  <div className="w-[317px] h-[216px] bg-[#3EAED2] rounded-lg"></div>
+                  <div className="absolute top-[250px] left-[2px]">
+                    <div className="absolute w-[68px] h-[68px] bg-white rounded-full mt-[-200px] ml-[145px]  ">
+                      <div className="absolute">
+                        <img
+                          src="/images/a1.png"
+                          className="w-[33px] h-[33px] mt-[17px] ml-[19px]"
+                        />
+                      </div>
+                    </div>
+                    <div className="absolute mt-[-205px] ml-[230px] text-[18px] font-bold w-[70px] text-[#FFFFFF]">
+                      <p>Bước 3</p>
+                    </div>
+
+                    <div className="z-30 w-[100px] absolute  mt-[-156px] ml-[225px] text-[18px]  text-[#FFFFFF]">
+                      <p>Nộp lệ phí</p>
+                      <p>xét tuyển</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="-translate-x-2/4 translate-y-[58px] left-[] relative z-20 ">
+                  <div class="border-solid border-l-[#3EAED2] border-l-[150px] border-y-transparent border-y-[150px] border-r-0"></div>
+                </div>
+                <div className="mb-[150px] mt-[100px] mr-[30px] -translate-x-48 z-10 ">
+                  <div className="w-[317px] h-[216px] bg-[#2433A6] rounded-lg"></div>
+                  <div className="absolute top-[250px] left-[2px]">
+                    <div className="absolute w-[68px] h-[68px] bg-white rounded-full mt-[-200px] ml-[145px]  ">
+                      <div className="absolute">
+                        <img
+                          src="/images/a1.png"
+                          className="w-[33px] h-[33px] mt-[17px] ml-[19px]"
+                        />
+                      </div>
+                    </div>
+                    <div className="absolute mt-[-205px] ml-[230px] text-[18px] font-bold w-[70px] text-[#FFFFFF]">
+                      <p>Bước 4</p>
+                    </div>
+
+                    <div className="z-30 w-[100px] absolute  mt-[-156px] ml-[225px] text-[18px]  text-[#FFFFFF]">
+                      <p>Tham dự</p>
+                      <p>phỏng vấn</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="-translate-x-2/4 translate-y-[58px] right-[190px] relative">
+                  <div class="border-solid border-l-[#2433A6] border-l-[150px] border-y-transparent border-y-[150px] border-r-0"></div>
+                </div>
+              </div>
+              <div className="relative w-[281px] h-[51px] bg-[#2433A6] rounded-[5px] text-[#FFFFFF] text-[20px]  flex justify-center items-center left-2/4 -translate-x-2/4 font-mont font-bold">
+                <h3 className="absolute ">THÔNG TIN CHUNG</h3>
+              </div>
+
+              <div className="relative grid grid-cols-10 gap-0 pt-[30px] pb-[30px] w-[1295px] text-[20px] leading-7 left-2/4 -translate-x-2/4  ">
+                <div className="relative col-span-5 w-[612px] ">
+                  <ul className="list-disc text-[#000000] pl-[100px] ">
+                    <li>Hình thức học trực tiếp vào các buổi tối trong tuần</li>
+                    <li>Sĩ số: 25-30 học viên/ lớp</li>
+                    <li>
+                      Ngành nghề có thể học khóa học: Tất cả các ngành nghề
+                    </li>
+                    <li>
+                      Hình thức xét tuyển: Xét duyệt hồ sơ và phỏng vấn tiếng
+                      Anh
+                    </li>
+                  </ul>
+                </div>
+                <div className="relative col-span-5 w-[612px]  left-2/4 -translate-x-2/4">
+                  <ul className="list-disc text-[#000000] ">
+                    <li>Ngôn ngữ đào tạo: Tiếng Anh</li>
+                    <li>
+                      Hạn nộp hồ sơ: 17/06/2022 (đăng ký sớm để có cơ hội trúng
+                      tuyển & phỏng vấn trước cho đến khi đủ chỉ tiêu)
+                    </li>
+                    <li>Ngày phỏng vấn dự kiến: Tháng 7/2022</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-[30px] mb-[100px]">
+        <div className="relative mt-[200px] h-[802px] bg-[url('/public/images/thanhtuu2.png')] bg-cover bg-center bg-origin-content">
+          <div className="container relative w-[2080px] left-2/4 -translate-x-2/4">
+            <div>
+              <div className="relative text-[25px] font-bold flex justify-center items-center pt-[48px] pb-[48px] -translate-y-[100px]">
+                <h3 className="absolute">
+                  <span className="text-[#2433A6]">GIỚI THIỆU</span>
+                  <span className="text-[#F5882E]"> TRƯỜNG</span>
+                </h3>
+              </div>
+              <div>
+                <div className="flex left-0 relative -translate-x-1/4">
+                  <div className=" bg-white w-[480px] h-[700px]  mr-[30px]  relative">
+                    <div className=" relative left-5 ">
+                      <img
+                        src="images/logo.png"
+                        className="absolute w-[198px] h-[60px] left-[70px] mt-[38px]"
+                      />
+                    </div>
+                    <div className="ml-[70px] mr-[62px] flex text-justify">
+                      <div className=" mt-[130px] relative">
+                        <h3 className="text-[18px] text-[#2433A6] font-bold">
+                          TRƯỜNG LIÊN KẾT
+                        </h3>
+                        <h3 className="text-[18px] text-[#2433A6] font-bold">
+                          JEAN MOULIN LYON LYON 3 (PHÁP)
+                        </h3>
+                        <p className="text-[16px] text-[#333333] mt-[20px]">
+                          Trường Đại học Jean Moulin Lyon 3 là một trong những
+                          trường Đại học đào tạo về kinh tế – luật uy tín nhất
+                          nước Pháp. Trường tọa lạc ở trung tâm của Lyon - thành
+                          phố được công nhận Di sản Thế giới của UNESCO và là
+                          thành phố đại học lớn thứ hai ở Pháp. Trường cung cấp
+                          các chương trình định hướng nghề nghiệp trong khuôn
+                          khổ học tập chất lượng cao có lợi cho sự phát triển
+                          trí tuệ và cá nhân. Với hàng nghìn học viên quốc tế
+                          được chào đón mỗi năm, nhiều dự án hợp tác trên toàn
+                          thế giới và các dự án hợp tác khoa học với các trường
+                          đại học nước ngoài, Đại học Jean Moulin Lyon 3 thể
+                          hiện cam kết quốc tế hóa bằng cách thúc đẩy tính di
+                          động của học viên và đội ngũ giảng viên. Trường được
+                          xếp hạng 4 cành cọ trong hệ thống các trường đào tạo
+                          về kinh doanh. Thạc Sỹ Tài Chính triển khai tại trường
+                          Đại học Thương mại đã được Bộ Giáo dục & Đào tạo Việt
+                          Nam cấp phép từ năm 2008.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" bg-white w-[480px] h-[700px]  mr-[30px]  relative">
+                    <div className=" relative left-5 ">
+                      <img
+                        src="images/logo2.png"
+                        className="absolute w-[152px] h-[99px] left-[68px] mt-[26px]"
+                      />
+                    </div>
+                    <div className="ml-[70px] mr-[62px] flex text-justify">
+                      <div className=" mt-[150px] relative">
+                        <h3 className="text-[18px] text-[#2433A6] font-bold">
+                          VIỆN ĐÀO TẠO QUỐC TẾ -{" "}
+                        </h3>
+                        <h3 className="text-[18px] text-[#2433A6] font-bold">
+                          TRƯỜNG ĐẠI HỌC THƯƠNG MẠI
+                        </h3>
+                        <p className="text-[16px] text-[#333333] mt-[20px]">
+                          Trường Đại học Thương mại thành lập năm 1960, có cơ sở
+                          chính đóng tại số 79 đường Hồ Tùng Mậu, quận Cầu Giấy,
+                          thành phố Hà Nội với tổng diện tích 380.000 m2. Trường
+                          là một trong những đại học có cảnh quan và khuôn viên
+                          đẹp nhất trong các trường đóng tại Hà Nội. Trường Đại
+                          học Thương mại là trường đại học chất lượng cao đa
+                          ngành, đa lĩnh vực. Từ khi thành lập đến nay, nhà
+                          trường đã đào tạo cung cấp cho xã hội hàng chục nghìn
+                          cử nhân kinh tế, hàng nghìn thạc sỹ, tiến sỹ kinh tế;
+                          bồi dưỡng nhiều cán bộ quản lý kinh tế cho ngành
+                          thương mại và các ngành khác, đã trực tiếp thực hiện
+                          và tham gia nhiều đề tài nghiên cứu khoa học cấp nhà
+                          nước, hàng trăm đề tài nghiên cứu khoa học cấp bộ và
+                          hàng chục hợp đồng nghiên cứu với các cơ quan, doanh
+                          nghiệp được Nhà nước, xã hội và cộng đồng doanh nghiệp
+                          đánh giá ngày càng cao.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
-
-export default App;
